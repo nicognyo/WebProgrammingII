5c2daa6e2574a8a345b602f746a2b1534bd57875
nicognyo <gnniyo345@gmail.com>
2023-09-28 16:11:47 +0700

    Tugas P1 WebProII

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,9 @@
+<?php
+class contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<center><h1>Hai Semuanya!</h1>";
+        echo "Nama saya Nico Genniyo<br>Saya tinggal di Jakarta Utara<br>Saya senang belajar pemrograman";
+    }
+}
